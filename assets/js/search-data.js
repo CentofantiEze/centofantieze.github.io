074d4f65acac4bd63eaa2faa7eb1e7c057290f20
CentofantiEze <ezecentofanti@gmail.com>
2025-09-25 14:11:11 +0000

    Deploying to gh-pages from @ CentofantiEze/centofantieze.github.io@ebe0dca4134dcdc54d3be65fba00a8bd8c64d5d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,8 +31,8 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          title: "Repositories",
+          description: "All my code is open source and available on GitHub.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
